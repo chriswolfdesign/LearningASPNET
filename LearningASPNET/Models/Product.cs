92395f974611d00566cd227d80b58c855fb0e345
cwolf <cwolf@quoininc.com>
2020-08-21 15:13:39 -0400

    Added a service

---
+++ LearningASPNET/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace LearningASPNET.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Maker { get; set; }
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+        public string MyProperty { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int[] Ratings { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Product>(this);
+        }
+    }
+}
